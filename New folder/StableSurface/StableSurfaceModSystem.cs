d7677d185e45686f4efc5e3d82daa692e60ca778
Wafers <67214441+nillawafers4u@users.noreply.github.com>
2024-10-23 08:52:47 -0600

    Added orginial vs code project

---
+++ New folder/StableSurface/StableSurfaceModSystem.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace StableSurface
+{
+    public class StableSurfaceModSystem : ModSystem
+    {
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            this.harmony = new Harmony("stablesurface");
+            this.harmony.PatchAll();
+        }
+
+        public override void Dispose()
+        {
+            Harmony harmony = this.harmony;
+            if (harmony != null)
+            {
+                harmony.UnpatchAll("stablesurface");
+            }
+        }
+
+        private Harmony harmony;
+    }
+}
---
+++ New folder/StableSurface/SystemTemporalStabilityPatch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System;
+using Vintagestory.API.Common;
+using Vintagestory.GameContent;
+using Vintagestory.ServerMods;
+
+namespace StableSurface
+{
+    [HarmonyPatch(typeof(SystemTemporalStability))]
+    [HarmonyPatch("GetTemporalStability",
+    new Type[] { typeof(double), typeof(double), typeof(double) })] 
+    //specifying which method of GetTemporalStability to change
+    public class SystemTemporalStabilityPatch : ModSystem
+    {       
+
+
+        public static void Postfix(ref float __result, ref double x, ref double y, ref double z, SystemTemporalStability __instance)
+        {
+
+
+            TemporalStormRunTimeData instanceStormData = __instance.StormData; 
+            //grabbing the SystemTemporalStability instance to use nowStormActive
+
+            if (y >= TerraGenConfig.seaLevel - 10 && instanceStormData.nowStormActive == false) 
+                //checking to see is the given y location is above the worlds sea level - 10. As well as Whether or not a temporal storm is active
+            {
+                __result = 1.5f; // Set the result to 1.5f when y is greater than or equal to seaLevel
+            }
+        }
+
+    }
+
+
+
+}
+
