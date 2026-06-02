53e2c6d330e5591112d15520313fa6b46fee6b41
Wafers <rsjonah@gmail.com>
2026-06-01 22:56:11 -0400

    got rid of some old messy files, updated to dotnet 10

---
+++ StableSurface/StableSurface/StableSurfaceModSystem.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace StableSurface
+{
+    public class StableSurfaceModSystem : ModSystem
+    {
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            this.harmony = new Harmony("stablesurface");
+            this.harmony.PatchAll();
+        }
+
+        public override void Dispose()
+        {
+            Harmony harmony = this.harmony;
+            if (harmony != null)
+            {
+                harmony.UnpatchAll("stablesurface");
+            }
+        }
+
+        public required Harmony harmony;
+    }
+}
